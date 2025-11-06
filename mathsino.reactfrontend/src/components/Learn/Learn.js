f5468dd302a7f7d560e078148c17de3e88e10287
Daniel Pasternak <ds.pasternak@onet.pl>
2025-11-07 00:26:22 +0100

    poprawki

--- mathsino.reactfrontend/src/components/Learn/Learn.js
+++ mathsino.reactfrontend/src/components/Learn/Learn.js
@@ -4,36 +4,78 @@ import styles from './Learn.module.css';
 
 
 const cardData = [
-  { 
+ { 
     topic: 'wave', 
     alt: 'wave', 
     image: lekcja1, 
-    title: 'Karta Fal', 
-    subtitle: 'Zdjęcie fali', 
-    description: 'To jest niestandardowy opis dla pierwszej karty o falach.' 
+    title: 'Lekcja 1',
+    subtitle: 'Podtytuł lekcji 1',
+    description: 'Opis lekcji 1'
   },
-  { 
+ { 
     topic: 'beach', 
     alt: 'beach', 
     image: lekcja1, 
-    title: 'Karta Plaży', 
-    subtitle: 'Zdjęcie plaży', 
-    description: 'Tutaj jest zupełnie inny opis, dotyczący plaży.'
+    title: 'Lekcja 2',
+    subtitle: 'Podtytuł lekcji 2',
+    description: 'Opis lekcji 2'
   },
-  { 
+ { 
     topic: 'mountain', 
     alt: 'mountain', 
     image: lekcja1, 
-    title: 'Card title', 
-    subtitle: 'Image from unsplash.com', 
-    description: 'This grid is an attempt to make something nice that works on touch devices.'
+    title: 'Lekcja 3',
+    subtitle: 'Podtytuł lekcji 3',
+    description: 'Opis lekcji 3'
   },
-  { topic: 'field', alt: 'field', image: lekcja1, title: 'Card title', subtitle: 'Image from unsplash.com', description: 'This grid is an attempt...'},
-  { topic: 'water', alt: 'water', image: lekcja1, title: 'Card title', subtitle: 'Image from unsplash.com', description: 'This grid is an attempt...'},
-  { topic: 'river', alt: 'river', image: lekcja1, title: 'Card title', subtitle: 'Image from unsplash.com', description: 'This grid is an attempt...'},
-  { topic: 'kite', alt: 'kite', image: lekcja1, title: 'Card title', subtitle: 'Image from unsplash.com', description: 'This grid is an attempt...'},
-  { topic: 'underwater', alt: 'underwater', image: lekcja1, title: 'Card title', subtitle: 'Image from unsplash.com', description: 'This grid is an attempt...'},
-  { topic: 'desert', alt: 'desert', image: lekcja1, title: 'Card title', subtitle: 'Image from unsplash.com', description: 'This grid is an attempt...'},
+ { 
+    topic: 'field', 
+    alt: 'field', 
+    image: lekcja1, 
+    title: 'Lekcja 4',
+    subtitle: 'Podtytuł lekcji 4',
+    description: 'Opis lekcji 4'
+  },
+ { 
+    topic: 'water', 
+    alt: 'water', 
+    image: lekcja1, 
+    title: 'Lekcja 5',
+    subtitle: 'Podtytuł lekcji 5',
+    description: 'Opis lekcji 5'
+  },
+ { 
+    topic: 'river', 
+    alt: 'river', 
+    image: lekcja1, 
+    title: 'Lekcja 6',
+    subtitle: 'Podtytuł lekcji 6',
+    description: 'Opis lekcji 6'
+  },
+ { 
+    topic: 'kite', 
+    alt: 'kite', 
+    image: lekcja1, 
+    title: 'Lekcja 7',
+    subtitle: 'Podtytuł lekcji 7',
+    description: 'Opis lekcji 7'
+  },
+ { 
+    topic: 'underwater', 
+    alt: 'underwater', 
+    image: lekcja1, 
+    title: 'Lekcja 8',
+    subtitle: 'Podtytuł lekcji 8',
+    description: 'Opis lekcji 8'
+  },
+ { 
+    topic: 'desert', 
+    alt: 'desert', 
+    image: lekcja1, 
+    title: 'Lekcja 9',
+    subtitle: 'Podtytuł lekcji 9',
+    description: 'Opis lekcji 9'
+  },
 ];
 
 
