27b1b4ec6586af8d8e15ef9234aca192ac21d666
OliwierGoluda <oliwier.goluda@gmail.com>
2025-11-12 12:19:17 +0100

    delete blazor frontend

--- Mathsino.host/AppHost.cs
+++ Mathsino.host/AppHost.cs
@@ -1,20 +1,12 @@
 using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var backend = builder.AddProject<Mathsino_Backend>("backend");
 
-var frontend = builder
-    .AddProject<Mathsino_Frontend>("frontend")
+builder
+    .AddNpmApp(name: "reactfrontend", workingDirectory: "../mathsino.reactfrontend")
     .WithReference(backend)
-    .WaitFor(backend);
-
-
-builder.AddNpmApp(
-        name: "reactfrontend", 
-        workingDirectory: "../mathsino.reactfrontend") 
-    .WithReference(backend) 
-    .WithHttpEndpoint(
-        targetPort: 3000,
-        name: "react-http");
+    .WithHttpEndpoint(targetPort: 3000, name: "react-http");
 
 builder.Build().Run();
