4ef4f19e308a5a4abbf604a1534db21be30f8e5b
Daniel Pasternak <ds.pasternak@onet.pl>
2025-12-04 00:36:11 +0100

    cały sprint

---
+++ Mathsino.Backend/Migrations/20251203231859_AddBalanceToUser.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mathsino.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBalanceToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Provider = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ProviderId = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Balance = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SingleGames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GameId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    PlayerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    SingleGameResult = table.Column<int>(type: "integer", nullable: true),
+                    SingleGameSplitResult = table.Column<int>(type: "integer", nullable: true),
+                    BalanceAfterGame = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SingleGames", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SingleGames_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserFriends",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    FriendId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFriends", x => new { x.UserId, x.FriendId });
+                    table.ForeignKey(
+                        name: "FK_UserFriends_Users_FriendId",
+                        column: x => x.FriendId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserFriends_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Balance", "Email", "FirstName", "LastName", "Provider", "ProviderId" },
+                values: new object[,]
+                {
+                    { 1, 5000, "alice.smith@example.com", "Alice", "Smith", "", "" },
+                    { 2, 3000, "bob.johnson@example.com", "Bob", "Johnson", "", "" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserFriends",
+                columns: new[] { "FriendId", "UserId" },
+                values: new object[,]
+                {
+                    { 2, 1 },
+                    { 1, 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SingleGames_UserId",
+                table: "SingleGames",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFriends_FriendId",
+                table: "UserFriends",
+                column: "FriendId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SingleGames");
+
+            migrationBuilder.DropTable(
+                name: "UserFriends");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
